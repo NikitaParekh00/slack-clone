7b80672bbc5d9a04a3b3a783c503d6e30d45b3fe
Nikita Parekh <nikitaparekh00@gmail.com>
2022-04-03 14:05:03 +0530

    building chat box

--- src/components/ChatInput.js
+++ src/components/ChatInput.js
@@ -1,17 +1,32 @@
-import React from "react";
+import React, { useRef } from "react";
 import styled from "styled-components";
 import { Button } from "@material-ui/core";
+import { db } from "../firebase";
+import firebase from "firebase";
 
 function ChatInput({ channelName, channelId }) {
+  const inputRef = useRef(null);
+
   const sendMessage = (e) => {
     e.preventDefault();
+
+    if (!channelId) {
+      return false;
+    }
+
+    db.collection("rooms").doc(channelId).collection("messages").add({
+      message: inputRef.current.value,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
   };
 
   return (
     <ChatInputContainer>
       <form>
-        <input placeholder={`Message #ROOM`} />
-        <Button hidden type="submit" onClick={sendMessage}></Button>
+        <input ref={inputRef} placeholder={`Message #ROOM`} />
+        <Button hidden type="submit" onClick={sendMessage}>
+          SEND
+        </Button>
       </form>
     </ChatInputContainer>
   );
@@ -19,4 +34,26 @@ function ChatInput({ channelName, channelId }) {
 
 export default ChatInput;
 
-const ChatInputContainer = styled.div``;
+const ChatInputContainer = styled.div`
+  border-radius: 20px;
+
+  > form {
+    position: relative;
+    display: flex;
+    justify-content: center;
+  }
+
+  > form > input {
+    position: fixed;
+    bottom: 30px;
+    width: 60%;
+    border: 1px solid gray;
+    border-radius: 3px;
+    padding: 20px;
+    outline: none;
+  }
+
+  > form > button {
+    display: none !important;
+  }
+`;
